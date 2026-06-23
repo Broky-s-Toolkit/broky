d96ed0391d5427a62e24931d822051cc96e55606
dacanizares <dacanizares@outlook.com>
2026-06-22 22:37:31 -0500

    Fixed repo

--- run/build-linux.sh
+++ run/build-linux.sh
@@ -3,7 +3,7 @@
 mkdir -p bin/dbg-linux
 rm bin/dbg-linux/run > /dev/null 2>&1
 gcc -Wall -Wextra -Wunused-variable -Wunused-parameter \
-    src/main.c lib/raylib-media/rmedia.c \
+    src/main.c \
     -g -ggdb -o bin/dbg-linux/run -std=c99 -pedantic \
     -I/usr/local/include -Ilib/raylib-media \
     -lraylib -lm \
